8c07da1e515b93c7b806ca1771fd19e09788747f
jeperico <joaoeperico@gmail.com>
2025-05-14 19:43:35 -0300

    [feat] add imports && main

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 runmain:
-	gcc main.c utils/generate_table.c -o app && ./app
+	gcc main.c utils/generate_table.c utils/render_table.c -o app && ./app
--- main.c
+++ main.c
@@ -2,6 +2,11 @@
 #include "utils/utils.h"
 
 void main() {
+  renderTable();
+  renderMMC();
+  renderMDC();
+  renderX1(false);
+  renderX2(false);
   generateMMC(172, 2123);
   generateMDC(24, 15);
 }
